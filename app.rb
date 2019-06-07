f3f900a80521a1fc173a77db1c29fe62bd278571
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-07 11:55:59 -0400

    Done

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    pl = PigLatinizer.new
-    @piglatin = pl.piglatinize(params[:user_phrase])
+    input = PigLatinizer.new
+    @piglatin = input.piglatinize(params[:user_phrase])
     erb :results
   end
 
